a340701e898d4ac2d35c910808ed8b3f6eeb5efe
ysrivatsa <62874709+ysrivatsa@users.noreply.github.com>
2026-04-26 15:37:27 +0530

    db

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module to-do-api
 
 go 1.26.1
+
+require github.com/lib/pq v1.12.3
--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"to-do-api/routes"
+
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
+
+func connectDB() {
+	connStr := "host=todoapp.c9sgquaek4s9.ap-southeast-2.rds.amazonaws.com port=5432 user=postgres password=Srivatsa123 dbname=postgres sslmode=require"
+
+	fmt.Println("Opening DB connection...")
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Before Ping...")
+	err = db.Ping()
+	fmt.Println("After Ping...")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected Successfully WITH THE DB")
+}
 
 func main() {
 	fmt.Println("Server Started")
+	connectDB()
 	routes.RegisterRoutes()
 	fmt.Println("Server is listening in port :8080")
 	http.ListenAndServe(":8080", nil)
